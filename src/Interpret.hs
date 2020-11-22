67e68afec4ae79af70de0b92cfdc9b0eb5791b99
Jesse Holzman <jesseholzman96@gmail.com>
2020-11-22 12:31:32 -0500

    Add skeleton for running interpreter on AST

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -1,9 +1,9 @@
 module Interpret where
 
 import Prelude hiding (lookup)
-import Control.Monad.Reader (ReaderT)
-import Control.Monad.Except (ExceptT)
-import Data.Map.Strict (Map)
+import Control.Monad.Reader (ReaderT(runReaderT))
+import Control.Monad.Except (ExceptT, runExceptT)
+import Data.Map.Strict (Map, fromList, empty, lookup)
 import Data.List (intercalate)
 import qualified AbsSperg as AST
 
@@ -20,6 +20,7 @@ data Value
   | String String
   | List [Value]
   | Func Closure
+  | Void
 
 instance Show Value where
   show (Bool   b) = show b
@@ -29,3 +30,30 @@ instance Show Value where
   show (String s) = s
   show (List   l) = "[" ++ intercalate "," (map show l) ++ "]"
   show (Func   _) = "<function>"
+  show Void       = "<void>"
+
+
+topLevelEnv :: AST.Prog -> Env
+topLevelEnv (AST.Program stmts) = Env $ fromList (map toTuple stmts)
+  where toTuple (AST.SBind (AST.Ident id) expr) = (id, (expr, Env empty))
+
+
+entrypoint :: Env -> AST.Expr
+entrypoint (Env env) = case lookup "main" env of
+  Nothing       -> AST.ELit AST.LVoid   -- if no main function, just do nop
+  Just (exp, _) -> exp
+
+
+runInterpreter :: AST.Prog -> IO ()
+runInterpreter prog = do
+  let env  = topLevelEnv prog
+  let main = entrypoint env
+  res <- runReaderT (runExceptT (interpret main)) env
+  case res of
+    Left  err -> putStrLn $ "Error: " ++ err
+    Right val -> print val
+
+
+interpret :: AST.Expr -> Interp Value
+interpret _ = do
+  return Void
