6467442c126b480cebe2342c5f2311d615afaff1
Jesse Holzman <jesseholzman96@gmail.com>
2021-01-16 17:07:18 -0500

    Offload param lookup for builtins into separate function.

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -14,6 +14,22 @@ paramList :: Int -> [String]
 paramList n = ("p" ++) . show <$> [1 .. n]
 
 
+getParams :: Int -> Interp [Value]
+getParams n = do
+  let ps = paramList n
+  env <- lift ask
+  getPIter env ps
+ where
+  getPIter :: Store -> [String] -> Interp [Value]
+  getPIter _     []       = return []
+  getPIter store (p : ps) = do
+    case lookup p store of
+      Nothing -> throwError "Failed to fetch argument for builtin."
+      Just v  -> do
+        rest <- getPIter store ps
+        return $ v : rest
+
+
 builtinEnv :: Store
 builtinEnv = fromList
   [ ("head"   , Lambda (paramList 1) (BuiltIn bHead, empty))
@@ -30,36 +46,30 @@ builtinEnv = fromList
 -- | Get the first element of a list.
 bHead :: Interp Value
 bHead = do
-  let [p] = paramList 1
-  env <- lift ask
-  case lookup p env of
-    Nothing              -> throwError "Failed to fetch argument for builtin."
-    Just (List []      ) -> throwError "Cannot evaluate head of empty list."
-    Just (List (x : xs)) -> return x
-    Just _ -> throwError "Cannot evaluate head of something that's not a list."
+  [p] <- getParams 1
+  case p of
+    List [] -> throwError "Cannot evaluate head of empty list."
+    List (x : xs) -> return x
+    _ -> throwError "Cannot evaluate head of something that's not a list."
 
 
 -- | Get a list excluding its first element.
 bTail :: Interp Value
 bTail = do
-  let [p] = paramList 1
-  env <- lift ask
-  case lookup p env of
-    Nothing              -> throwError "Failed to fetch argument for builtin."
-    Just (List []      ) -> throwError "Cannot evaluate tail of empty list."
-    Just (List (x : xs)) -> return (List xs)
-    Just _ -> throwError "Cannot evaluate tail of something that's not a list."
+  [p] <- getParams 1
+  case p of
+    List [] -> throwError "Cannot evaluate tail of empty list."
+    List (x : xs) -> return $ List xs
+    _ -> throwError "Cannot evaluate tail of something that's not a list."
 
 
 -- | Count the number of elements in a list.
 bLength :: Interp Value
 bLength = do
-  let [p] = paramList 1
-  env <- lift ask
-  case lookup p env of
-    Nothing -> throwError "Failed to fetch argument for builtin."
-    Just (List l) -> return $ Int (toInteger $ length l)
-    Just _ -> throwError "Cannot evaluate tail of something that's not a list."
+  [p] <- getParams 1
+  case p of
+    List l -> return $ Int (toInteger $ length l)
+    _      -> throwError "Cannot evaluate tail of something that's not a list."
 
 
 
@@ -68,25 +78,17 @@ bLength = do
 -- | Print a value to stdout (no trailing newline).
 bPrint :: Interp Value
 bPrint = do
-  let [p] = paramList 1
-  env <- lift ask
-  case lookup p env of
-    Nothing -> throwError "Failed to fetch argument for builtin."
-    Just v  -> do
-      liftIO $ putStr $ show v
-      return Void
+  [p] <- getParams 1
+  liftIO $ putStr $ show p
+  return Void
 
 
 -- | Print a value to stdout, followed by a newline.
 bPrintln :: Interp Value
 bPrintln = do
-  let [p] = paramList 1
-  env <- lift ask
-  case lookup p env of
-    Nothing -> throwError "Failed to fetch argument for builtin."
-    Just v  -> do
-      liftIO $ print v
-      return Void
+  [p] <- getParams 1
+  liftIO $ print p
+  return Void
 
 
 -- | Read a line of input from stdin, return the line as a string.
