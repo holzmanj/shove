61b66f2830eed6c248b26815e6282213e06690a2
Jesse Holzman <jesseholzman96@gmail.com>
2021-01-16 09:02:40 -0500

    Implement builtins for basic user IO.

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -2,12 +2,13 @@ module Builtins where
 
 import Prelude hiding (lookup)
 import Data.Map (lookup, Map, empty, fromList)
-import Control.Monad.Trans (MonadTrans(lift))
+import Control.Monad.Trans (MonadIO(liftIO), MonadTrans(lift))
 import Control.Monad.Except (MonadError(throwError))
 import Control.Monad.Reader (MonadReader(ask))
 
 import qualified AbsGrammar as AST
-import Types (Value(Int, Lambda, List), Interp, Store, FuncBody(BuiltIn))
+import Types
+  (Value(String, Void, Int, Lambda, List), Interp, Store, FuncBody(BuiltIn))
 
 
 paramList :: Int -> [String]
@@ -16,11 +17,16 @@ paramList n = ("p" ++) . show <$> [1 .. n]
 
 builtinEnv :: Store
 builtinEnv = fromList
-  [ ("head"  , Lambda (paramList 1) (BuiltIn bHead, empty))
-  , ("tail"  , Lambda (paramList 1) (BuiltIn bTail, empty))
-  , ("length", Lambda (paramList 1) (BuiltIn bLength, empty))
+  [ ("head"   , Lambda (paramList 1) (BuiltIn bHead, empty))
+  , ("tail"   , Lambda (paramList 1) (BuiltIn bTail, empty))
+  , ("length" , Lambda (paramList 1) (BuiltIn bLength, empty))
+  , ("print"  , Lambda (paramList 1) (BuiltIn bPrint, empty))
+  , ("println", Lambda (paramList 1) (BuiltIn bPrintln, empty))
+  , ("readln" , Lambda [] (BuiltIn bReadln, empty))
   ]
 
+
+-- | List Operations
 
 bHead :: Interp Value
 bHead = do
@@ -52,3 +58,33 @@ bLength = do
     Nothing -> throwError "Failed to fetch argument for builtin."
     Just (List l) -> return $ Int (toInteger $ length l)
     Just _ -> throwError "Cannot evaluate tail of something that's not a list."
+
+
+-- | IO 
+
+bPrint :: Interp Value
+bPrint = do
+  let [p] = paramList 1
+  env <- lift ask
+  case lookup p env of
+    Nothing -> throwError "Failed to fetch argument for builtin."
+    Just v  -> do
+      liftIO $ putStr $ show v
+      return Void
+
+
+bPrintln :: Interp Value
+bPrintln = do
+  let [p] = paramList 1
+  env <- lift ask
+  case lookup p env of
+    Nothing -> throwError "Failed to fetch argument for builtin."
+    Just v  -> do
+      liftIO $ print v
+      return Void
+
+
+bReadln :: Interp Value
+bReadln = do
+  line <- liftIO getLine
+  return $ String line
